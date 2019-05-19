f12732ab9a53fe387ed7564488c22bc04acabd09
malvicamp <malvina.campion@gmail.com>
2019-05-18 17:06:43 -0700

    fix broken x button

--- js/malvina_home.js
+++ js/malvina_home.js
@@ -2,8 +2,3 @@
 particlesJS.load('particles-js', 'js/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
-
-$('#particles-js').click(function() { 
-    console.log('foooooo')
-    debugger; 
-})
